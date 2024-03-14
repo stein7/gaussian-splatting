29f3e9900ba25f21d9f5379aa3554ce285a9d3ac
stein7 <centralp7@gmail.com>
2024-03-14 08:24:11 +0000

    a

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 
-#CUDA_VISIBLE_DEVICES=2
+#CUDA_VISIBLE_DEVICES=2 // /home/sslunder0/project/NextProject/dataset_nerf_synthetic
 
-#python train.py -s /data1/dataset/nerf_synthetic/chair/ --model_path output/chair
-python render.py -m output/chair/ --eval --iteration 30000
-python render.py -m output/chair/ --eval --iteration 7000
-python metrics.py -m output/chair/
+#python3 train.py -s /home/sslunder0/project/NextProject/dataset_nerf_synthetic/chair --model_path output/chair
+python3 render.py -m output/chair/ --eval --iteration 30000
+python3 render.py -m output/chair/ --eval --iteration 7000
+python3 metrics.py -m output/chair/
