8d0bb2d2067eb30a7757d86f6d9802eb9b60203b
stein7 <centralp7@gmail.com>
2024-03-20 04:50:41 +0000

    f

--- script.sh
+++ script.sh
@@ -5,9 +5,9 @@
 # CUDA_VISIBLE_DEVICES=1 python3 train.py -s /home/sslunder0/project/NextProject/dataset_nerf_synthetic/chair --model_path output/chair_nerf_freq_2sig \
 #     --convert_SHs_python --compute_cov3D_python --encoder frequency
 
-CUDA_VISIBLE_DEVICES=1 python3 render.py -m output/chair_nerf_hash_2sig/ --eval --iteration 30000 --convert_SHs_python --compute_cov3D_python
-CUDA_VISIBLE_DEVICES=1 python3 render.py -m output/chair_nerf_hash_2sig/ --eval --iteration 7000 --convert_SHs_python --compute_cov3D_python
-CUDA_VISIBLE_DEVICES=1 python3 metrics.py -m output/chair_nerf_hash_2sig/
+# CUDA_VISIBLE_DEVICES=1 python3 render.py -m output/chair_nerf_hash_2sig/ --eval --iteration 30000 --convert_SHs_python --compute_cov3D_python
+# CUDA_VISIBLE_DEVICES=1 python3 render.py -m output/chair_nerf_hash_2sig/ --eval --iteration 7000 --convert_SHs_python --compute_cov3D_python
+# CUDA_VISIBLE_DEVICES=1 python3 metrics.py -m output/chair_nerf_hash_2sig/
 
 
 CUDA_VISIBLE_DEVICES=1 python3 render.py -m output/chair_nerf_freq_2sig/ --eval --iteration 30000 --convert_SHs_python --compute_cov3D_python
